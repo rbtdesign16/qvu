eeb07d2f67f044355eefc855b138cad9ef9ac36c
rbtucker <rbtucker16@gmail.com>
2023-06-16 14:18:39 -0500

    trying to get security in place

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,22 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Switch } from "react-router";
-import AuthState from "./context/auth/AuthState";
 import Home from "./components/pages/Home";
 
 
 const App = () => {
 
-  return (
-      <AuthState>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="*" component={Home} />
-            </Switch>
-          </Router>
-      </AuthState>
-  );
+    return (
+            <Router>
+                <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="*" component={Home} />
+                </Switch>
+            </Router>
+            );
 };
 
 export default App;
--- src/main/java/org/rbt/qvu/QvuConfig.java
+++ src/main/java/org/rbt/qvu/QvuConfig.java
@@ -19,7 +19,7 @@ public class QvuConfig implements WebMvcConfigurer {
  
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("/index");
+        registry.addViewController("/").setViewName("forward:/index.html");
     }
 
     @Override

35c378ecb3c0335b3440eed268cb79e98de4d8b7
rbtucker <rbtucker16@gmail.com>
2023-07-03 08:29:01 -0500

    start building query design

--- src/main/frontend/src/pages/querydesign/QueryDesign.jsx
+++ src/main/frontend/src/pages/querydesign/QueryDesign.jsx
@@ -7,11 +7,13 @@ import { Tabs, Tab } from "react-bootstrap";
 import useMessage from "../../context/MessageContext";
 import {INFO, WARN, ERROR} from "../../utils/helper";
 import { Splitter, SplitterPanel } from 'primereact/splitter';
+import DataSelectTree from "./DataSelectTree";
+
 const QueryDesign = () => {
     return (
                 <Splitter stateKey={"qdesign"} stateStorage={"local"} guttorSize={8}>
                     <SplitterPanel size={25} className="flex align-items-center justify-content-center">
-                        Panel 1
+                       <DataSelectTree/>
                     </SplitterPanel>
                     <SplitterPanel size={75} className="flex align-items-center justify-content-center">
                         Panel 2

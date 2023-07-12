07fc6a303a9557d1f1b95a8cb361c6d8e50fc597
rbtucker <rbtucker16@gmail.com>
2023-07-12 17:02:04 -0500

    get query design up and running

--- src/main/frontend/src/pages/querydesign/DataSelectTree.jsx
+++ src/main/frontend/src/pages/querydesign/DataSelectTree.jsx
@@ -4,11 +4,12 @@ import {FcTimeline, FcKey} from "react-icons/fc";
 import { FaSquare, FaCheckSquare, FaMinusSquare } from "react-icons/fa";
 import { IoMdArrowDropright } from "react-icons/io";
 import useQueryDesign from "../../context/QueryDesignContext";
-import useLang from "../../context/LangContext";
+import useLang from "../..//context/LangContext";
+import JoinType from "./JoinType";
 import cx from "classnames";
 import PropTypes from "prop-types";
 import {
-    NODE_TYPE_ROOT,
+NODE_TYPE_ROOT,
         NODE_TYPE_TABLE,
         NODE_TYPE_COLUMN,
         NODE_TYPE_IMPORTED_FOREIGNKEY,
@@ -18,15 +19,15 @@ import {
 const DataSelectTree = (props) => {
     const {getText} = useLang();
     const {
-        treeViewData, 
-        selectedColumnIds, 
-        selectedTableIds, 
-        baseTable, 
-        setTreeViewData, 
-        setSelectedColumnIds, 
-        setSelectedTableIds, 
+        treeViewData,
+        selectedColumnIds,
+        selectedTableIds,
+        baseTable,
+        setTreeViewData,
+        setSelectedColumnIds,
+        setSelectedTableIds,
         setBaseTable} = useQueryDesign();
-
+    const [showJoinType, setShowJoinType] = useState({show: false});
     const ArrowIcon = ({ isOpen, className }) => {
         const baseClass = "arrow";
         const classes = cx(
@@ -43,9 +44,9 @@ const DataSelectTree = (props) => {
             return <FaCheckSquare {...rest} />;
         } else {
             return <FaSquare {...rest} />;
-        }
+    }
     };
-    
+
     const getColumnLinks = (md) => {
         if (md.fromdiscols && md.todiscols) {
             return ": [" + md.fromdiscols + " -> " + md.todiscols + "]";
@@ -53,7 +54,7 @@ const DataSelectTree = (props) => {
             return "";
         }
     };
-    
+
     const getIcon = (element) => {
         if (element.metadata && element.metadata.type) {
             switch (String(element.metadata.type)) {
@@ -68,18 +69,26 @@ const DataSelectTree = (props) => {
                 case NODE_TYPE_COLUMN:
                     if (element.metadata.pk) {
                         return <FcKey className="icon-s" size={SMALL_ICON_SIZE}/>;
-                    } else {    
+                    } else {
                         return <FcTimeline className="icon-s" size={SMALL_ICON_SIZE}/>;
-                    }    
+                    }
                 case NODE_TYPE_IMPORTED_FOREIGNKEY:
                     if (selectedTableIds.includes(element.id)) {
-                        return <img src="table_imp_sel.png" width={SMALL_ICON_SIZE} height={SMALL_ICON_SIZE} />;
+                        if (element.metadata.jointype === "inner") {
+                            return <img src="table_imp_inn_sel.png" width={SMALL_ICON_SIZE} height={SMALL_ICON_SIZE} />;
+                        } else {
+                            return <img src="table_imp_sel.png" width={SMALL_ICON_SIZE} height={SMALL_ICON_SIZE} />;
+                        }
                     } else {
                         return <img src="table_imp.png" width={SMALL_ICON_SIZE} height={SMALL_ICON_SIZE} />;
                     }
                 case NODE_TYPE_EXPORTED_FOREIGNKEY:
                     if (selectedTableIds.includes(element.id)) {
-                        return <img src="table_exp_sel.png" width={SMALL_ICON_SIZE} height={SMALL_ICON_SIZE} />;
+                        if (element.metadata.jointype === "inner") {
+                            return <img src="table_exp_inn_sel.png" width={SMALL_ICON_SIZE} height={SMALL_ICON_SIZE} />;
+                        } else {
+                            return <img src="table_exp_sel.png" width={SMALL_ICON_SIZE} height={SMALL_ICON_SIZE} />;
+                        }    
                     } else {
                         return <img src="table_exp.png" width={SMALL_ICON_SIZE} height={SMALL_ICON_SIZE} />;
                     }
@@ -95,10 +104,11 @@ const DataSelectTree = (props) => {
         if (!isSelected) {
             if (element.metadata && element.metadata.roottable) {
                 if (!baseTable) {
-                    setBaseTable( element.metadata.roottable);
+                    setBaseTable(element.metadata.roottable);
                 }
-                
+
                 if (baseTable !== element.metadata.roottable) {
+                    clearJoinSettings(selectedColumnIds);
                     setSelectedNodeIds([element.id]);
                     setBaseTable(element.metadata.roottable);
                 } else {
@@ -106,7 +116,7 @@ const DataSelectTree = (props) => {
                     sids.push(element.id);
                     setSelectedNodeIds(sids);
                 }
-                    
+
             }
         } else { // unselect - remove id
             let sids = [...selectedColumnIds];
@@ -115,44 +125,79 @@ const DataSelectTree = (props) => {
                 sids.splice(indx, 1);
                 setSelectedNodeIds(sids);
             }
-        }   
- 
+        }
+
         handleSelect(e);
         e.stopPropagation();
     };
-    
+
     const setSelectedNodeIds = (ids) => {
         setSelectedColumnIds(ids);
         setSelectedTableIds(getSelectedTableIds(ids));
     };
+    
+    const clearJoinSettings = (ids) => {
+        for (let i = 0; i < selectedTableIds.length; ++i) {
+            treeViewData[selectedTableIds[i]].metadata.jointype = "";
+        }
+    }
 
     const getSelectedTableIds = (ids) => {
         let tset = new Set();
-        
+
         for (let i = 0; i < ids.length; ++i) {
             let pid = treeViewData[ids[i]].parent;
-           
+
             while (pid) {
                 tset.add(pid);
                 pid = treeViewData[pid].parent;
-             }
+            }
         }
-         
-        let retval = [...tset]; 
+
+        let retval = [...tset];
         return retval;
     };
     
+    const hideJoinType = () => {
+        setShowJoinType({show: false});
+    }
+    
+    const setJoinType = (joinType, nodeId) => {
+        treeViewData[nodeId].metadata.jointype = joinType;
+        hideJoinType();
+    }
+
+    const handleContextMenu = (e, element) => {
+        if (element && element.metadata) {
+            switch (String(element.metadata.type)) {
+                case NODE_TYPE_ROOT:
+                    break;
+                case NODE_TYPE_TABLE:
+                    break;
+                case NODE_TYPE_COLUMN:
+                    break;
+                case NODE_TYPE_IMPORTED_FOREIGNKEY:
+                case NODE_TYPE_EXPORTED_FOREIGNKEY:
+                    if (selectedTableIds.includes(element.id)) {
+                        e.preventDefault();
+                        setShowJoinType({show: true, setJoinType: setJoinType, joinType: element.metadata.jointype, nodeId: element.id, hide: hideJoinType});
+                     }
+                    break;
+            }
+        }
+    }
+
     const getNode = (element, handleSelect, isSelected, isBranch, isExpanded) => {
         if (isBranch) {
-            return <span><ArrowIcon isOpen={isExpanded}/><span className="name">{getIcon(element)}{element.name}{getColumnLinks(element.metadata)}</span></span>;
-         } else {
-             return <span><CheckBoxIcon
-                className="checkbox-icon"
-                onClick={(e) => onClick(e, element, handleSelect, isSelected)}
-                checked={isSelected}/><span className="name" onClick={(e) => onClick(e, element, handleSelect, isSelected)}>{getIcon(element)}{element.name}</span></span>;
-         }       
+            return <span><ArrowIcon isOpen={isExpanded}/><span onContextMenu={(e) => handleContextMenu(e, element)} className="name">{getIcon(element)}{element.name}{getColumnLinks(element.metadata)}</span></span>;
+        } else {
+            return <span><CheckBoxIcon
+                    className="checkbox-icon"
+                    onClick={(e) => onClick(e, element, handleSelect, isSelected)}
+                    checked={isSelected}/><span className="name" onClick={(e) => onClick(e, element, handleSelect, isSelected)}>{getIcon(element)}{element.name}</span></span>;
+        }
     };
-    
+
     const nodeRenderer = (p) => {
         const {element,
             isBranch,
@@ -163,16 +208,17 @@ const DataSelectTree = (props) => {
             handleSelect,
             handleExpand} = p;
         return <div
-            {...getNodeProps({onClick: handleExpand})}
-            style={{marginLeft: 40 * (level - 1)}}
-            >
-            {getNode(element, handleSelect, isSelected, isBranch, isExpanded )}
-
-        </div>
+        {...getNodeProps({onClick: handleExpand})}
+        style={{marginLeft: 40 * (level - 1)}}
+        >
+        {getNode(element, handleSelect, isSelected, isBranch, isExpanded)}
+    
+    </div>
     };
 
     if (treeViewData) {
         return (<div className="tree-view">
+        <JoinType config={showJoinType}/>
             <TreeView
                 data={treeViewData}
                 propagateCollapse={true}
---
+++ src/main/frontend/src/pages/querydesign/JoinType.jsx
@@ -0,0 +1,50 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/reactjs.jsx to edit this template
+ */
+import React, {useState} from "react";
+import PropTypes from "prop-types";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button"
+import useLang from "../../context/LangContext";
+import {MODAL_TITLE_SIZE} from "../../utils/helper";
+
+const JoinType = (props) => {
+    const {getText} = useLang();
+    const {config} = props;
+    const [joinType, setJoinType] = useState(config.joinType ? config.joinType : "outer");
+
+    const isInner = () => {
+        return (joinType === "inner");
+    };
+    
+    return (
+            <div className="static-modal">
+                <Modal animation={false} 
+                       size="sm"
+                       show={config.show} 
+                       backdrop={true} 
+                       keyboard={true}>
+                    <Modal.Header>
+                        <Modal.Title as={MODAL_TITLE_SIZE}>{getText("Join Type")}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div onChange={(e) => setJoinType(e.target.value)}>
+                            <input type="radio" name="jointype" checked={!isInner()} value="outer"/>&nbsp;&nbsp;{getText("Outer Join")}<br />
+                            <input type="radio" name="jointype" checked={isInner()} value="inner"/>&nbsp;&nbsp;{getText("Inner Join")}
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button size="sm" onClick={() => config.hide()}>Cancel</Button>
+                        <Button size="sm" variant="primary" type="submit" onClick={(e) => config.setJoinType(joinType, config.nodeId)}>{getText("Ok")}</Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+                    );
+        };
+
+        JoinType.propTypes = {
+            config: PropTypes.object.isRequired
+        };
+
+        export default JoinType;

991dbc8f2cf3d57b44b6e03de14f7d6a0d2cb83b
rbtucker <rbtucker16@gmail.com>
2023-11-04 12:04:27 -0500

    build report logic

---
+++ src/main/java/org/rbtdesign/qvu/dto/Rect.java
@@ -0,0 +1,44 @@
+package org.rbtdesign.qvu.dto;
+
+/**
+ *
+ * @author rbtuc
+ */
+public class Rect {
+    private double left;
+    private double top;
+    private double width;
+    private double height;
+
+    public double getLeft() {
+        return left;
+    }
+
+    public void setLeft(double left) {
+        this.left = left;
+    }
+
+    public double getTop() {
+        return top;
+    }
+
+    public void setTop(double top) {
+        this.top = top;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
--- src/main/java/org/rbtdesign/qvu/dto/ReportObject.java
+++ src/main/java/org/rbtdesign/qvu/dto/ReportObject.java
@@ -7,26 +7,9 @@ import org.rbtdesign.qvu.util.Constants;
  * @author rbtuc
  */
 public class ReportObject {
-    private double width = 0;
-    private double height = 0;
+    private Rect position = new Rect();
     private int location = Constants.REPORT_LOCATION_BODY;
     private int REPORT_OBJECT_TYPE = Constants.REPORT_OBJECT_TYPE_TEXT;
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
 
     public int getLocation() {
         return location;
@@ -42,6 +25,14 @@ public class ReportObject {
 
     public void setREPORT_OBJECT_TYPE(int REPORT_OBJECT_TYPE) {
         this.REPORT_OBJECT_TYPE = REPORT_OBJECT_TYPE;
+    }
+
+    public Rect getPosition() {
+        return position;
+    }
+
+    public void setPosition(Rect position) {
+        this.position = position;
     }
     
     

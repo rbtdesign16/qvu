cc35f1fa80f8b60deb35bc058e642d490a3e401e
rbtucker <rbtucker16@gmail.com>
2023-11-09 20:17:20 -0600

    build report logic

--- src/main/frontend/src/pages/reportdesign/ReportRuler.jsx
+++ src/main/frontend/src/pages/reportdesign/ReportRuler.jsx
@@ -1,74 +1,105 @@
 import React, {useEffect, useRef} from "react";
 import PropTypes from "prop-types";
 import {
-    HORIZONTAL_KEY,
-    VERTICAL_KEY,
-    REPORT_UNITS_MM,
-    PIXELS_PER_MM,
-    PIXELS_PER_INCH
-} from "../../utils/helper";
+HORIZONTAL_KEY,
+        VERTICAL_KEY,
+        REPORT_UNITS_MM,
+        PIXELS_PER_MM,
+        PIXELS_PER_INCH
+        } from "../../utils/helper";
 
 const ReportRuler = (props) => {
     const {report, height, width, type} = props;
-    
- 
-    const drawUnits = () => {
+
+
+    const drawRuler = () => {
         let retval = [];
+        let unitLength;
+        let className;
         if (report.pageUnits === REPORT_UNITS_MM) {
             let inc = PIXELS_PER_MM * 5;
-            let curpos = inc ;
-        } else {
-            let inc = Number(PIXELS_PER_INCH / 4);
-            
+            let curpos = inc;
+            unitLength = ["15px", "5px"];
+
             if (type === HORIZONTAL_KEY) {
-                let curpos = inc;
                 do {
                     retval.push(curpos + "px");
                     curpos += inc;
                 } while (curpos < width);
+
+                className = "hrule-unit";
             } else {
-                
+                do {
+                    retval.push(curpos + "px");
+                    curpos += inc;
+                } while (curpos < height);
+
+                className = "vrule-unit";
+            }
+        } else {
+            let inc = Number(PIXELS_PER_INCH / 4);
+            unitLength = ["15px", "5px", "10px", "5px"];
+            let curpos = inc;
+
+            if (type === HORIZONTAL_KEY) {
+                do {
+                    retval.push(curpos + "px");
+                    curpos += inc;
+                } while (curpos < width);
+
+                className = "hrule-unit";
+            } else {
+                do {
+                    retval.push(curpos + "px");
+                    curpos += inc;
+                } while (curpos < height);
+                className = "vrule-unit";
             }
         }
-        
+
         if (retval.length > 0) {
+            let num = 1;
             return retval.map((p, indx) => {
-                let h;
-                switch (indx % 4) {
-                    case 0:
-                        h = "15px";
-                        break;
-                    case 1:
-                    case 3:
-                        h = "5px";
-                        break;
-                    case 2:
-                        h = "10px";
-                        break;
-                }
-                return <span className="ruler-hunit" style={{left: p, height: h}}></span>;
+                let lpos = ((indx + 1) % unitLength.length);
+
+/*
+                if (lpos === 0) {
+                    if (type === HORIZONTAL_KEY) {
+                        return <div className={className} style={{left: p, height: unitLength[lpos]}}></div>;
+                    } else {
+                        return <div className={className} style={{left: p, width: unitLength[lpos]}}></div>;
+                    }
+                } else {
+                 * */
+ 
+                    if (type === HORIZONTAL_KEY) {
+                        return <div className={className} style={{left: p, height: unitLength[lpos]}}></div>;
+                    } else {
+                        return <div className={className} style={{top: p, width: unitLength[lpos]}}></div>;
+                    }
+              //  }
             });
         } else {
             return "";
         }
     };
-    
+
     const buildRuler = () => {
         let myStyle = {
             height: height + "px",
             width: width + "px"
         };
-        
+
         if (type === VERTICAL_KEY) {
             myStyle.left = 0;
         } else {
             myStyle.top = 0;
             myStyle.marginLeft = height + "px";
         }
-        
-        return <div style={myStyle} className="ruler"></div>;
+
+        return <div style={myStyle} className="ruler">{drawRuler()}</div>;
     };
-    
+
     return buildRuler();
 };
 

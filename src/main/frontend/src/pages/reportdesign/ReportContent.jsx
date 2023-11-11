37ba7d54235bde1638e5396fd54a8ba5aef94467
rbtucker <rbtucker16@gmail.com>
2023-11-10 23:08:12 -0600

    build report logic

--- src/main/frontend/src/pages/reportdesign/ReportContent.jsx
+++ src/main/frontend/src/pages/reportdesign/ReportContent.jsx
@@ -1,21 +1,23 @@
 import React from "react";
+import { Splitter, SplitterPanel } from 'primereact/splitter';
 import PropTypes from "prop-types";
-import { 
-    REPORT_ORIENTATION_LANDSCAPE, 
-    REPORT_ORIENTATION_PORTRAIT,
-    REPORT_UNITS_INCH,
-    REPORT_UNITS_MM
+import {
+REPORT_ORIENTATION_LANDSCAPE,
+        REPORT_ORIENTATION_PORTRAIT,
+        REPORT_UNITS_INCH,
+        REPORT_UNITS_MM,
+        SPLITTER_GUTTER_SIZE
 } from "../../utils/helper";
 
 const ReportContent = (props) => {
     const {report, reportSettings} = props;
-    
+
     const getStyle = () => {
         let height;
         let width;
-        
+
         let size = reportSettings.pageSizeSettings[report.pageSize];
-        
+
         if (report.pageOrientation === REPORT_ORIENTATION_PORTRAIT) {
             if (report.pageUnits === REPORT_UNITS_INCH) {
                 width = size[2] + "in";
@@ -23,7 +25,7 @@ const ReportContent = (props) => {
             } else {
                 width = size[0] + "mm";
                 height = size[1] + "mm";
-            }   
+            }
         } else {
             if (report.pageUnits === REPORT_UNITS_INCH) {
                 width = size[3] + "in";
@@ -31,18 +33,32 @@ const ReportContent = (props) => {
             } else {
                 width = size[1] + "mm";
                 height = size[0] + "mm";
-            }   
+            }
         }
-        
-        
+
+
         return {
             width: width,
             height: height,
             top: "-" + height
         };
     };
-    
-    return <div style={getStyle()} className="report-content"></div>;
+
+    return <div style={getStyle()} className="report-content">
+        <Splitter style={{border: "none", width: "100%", height: "100%"}} 
+            layout="vertical" stateKey={"report"} 
+            stateStorage={"local"} 
+            gutterSize={SPLITTER_GUTTER_SIZE / 2}>
+            <SplitterPanel size={10}>
+                <div>header</div>
+            </SplitterPanel>
+            <SplitterPanel size={80} >
+                <div>body</div>
+            </SplitterPanel>
+            <SplitterPanel size={10} >
+                <div>footer</div>
+            </SplitterPanel>
+        </Splitter></div>;
 };
 
 
